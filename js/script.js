9fbf16f91f844603a944ff3ee74d603a467d18b1
ccnfrank <ccnfrank@icloud.com>
2022-08-17 18:14:54 +0200

    hjfhjfhj

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
-var manda = document.getElementById("ini");
-console.log(manda);
 
 
-manda.textContent = 'n0/0:local/0:pro/0:tables/0:oncloudovo text';
+var soma = document.querySelectorAll('li');
+console.log(soma);
+
+
+
+
+
