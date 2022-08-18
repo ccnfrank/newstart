74c5c9eca79874ecc018504024ac2ae9cd826706
ccnfrank <ccnfrank@icloud.com>
2022-08-18 20:48:36 +0200

    hjk

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 
 
+const user = document.querySelector('#user');
+const btn = document.querySelector('#btn');
+const resp = document.querySelector('.resp');
 
-var soma = document.querySelectorAll('li');
-console.log(soma);
 
+function fn() {
+
+    resp.textContent = user.value;
+
+};
+
+
+btn.addEventListener('click', fn);
+
+
+
+var ccc = '154612134546543';
+
+const arr = Array.from(ccc, Number);
+console.log(arr);
 
 
 
